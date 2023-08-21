1f86566c423987164c5971092dc7f0f80bac04b8
AngeloJ123 <104799675+AngeloJ123@users.noreply.github.com>
2023-08-21 12:13:42 -0400

    Merge branch 'main' of https://github.com/AngeloJoudah/FPBESpringBoot.git

--- src/main/java/com/example/config/SecurityConfiguration.java
+++ src/main/java/com/example/config/SecurityConfiguration.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.example.config.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+	
+	private final JwtAuthenticationFilter jwtAuthFilter;
+	
+	private final AuthenticationProvider authenticationProvider;
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,7 +25,14 @@ public class SecurityConfiguration {
 		.requestMatchers("")
 		.permitAll()
 		.anyRequest()
-		.authenticated();
+		.authenticated()
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authenticationProvider(authenticationProvider)
+		.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+		
 		return http.build();
 		
 	}

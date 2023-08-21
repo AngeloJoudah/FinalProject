d3b6c59c75416c521da83e9942d335d57b8dbfd9
AngeloJ123 <104799675+AngeloJ123@users.noreply.github.com>
2023-08-21 12:08:02 -0400

    security config

--- src/main/SecurityConfiguration.java
+++ src/main/SecurityConfiguration.java
@@ -1,5 +1,25 @@
 package main;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf()
+		.disable()
+		.authorizeHttpRequests()
+		.requestMatchers("")
+		.permitAll()
+		.anyRequest()
+		.authenticated();
+		return http.build();
+		
+	}
 }

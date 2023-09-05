d035111731b6855f150025787adb9d966e7145ba
AngeloJ123 <104799675+AngeloJ123@users.noreply.github.com>
2023-09-05 15:35:30 -0400

    change allowed origins in corsmappings

--- src/main/java/com/example/CorsConfig.java
+++ src/main/java/com/example/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        		.allowedOrigins("*")
+        		.allowedOrigins("https://finalprojectangelo.azurewebsites.net/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)

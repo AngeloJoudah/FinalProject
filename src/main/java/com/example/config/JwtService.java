4a2a7ce50ca02c0c96967c8ec84835c13286686b
AngeloJ123 <104799675+AngeloJ123@users.noreply.github.com>
2023-09-06 11:07:55 -0400

    Extend token to 30 minutes

--- src/main/java/com/example/config/JwtService.java
+++ src/main/java/com/example/config/JwtService.java
@@ -38,7 +38,7 @@ public class JwtService {
 				.setClaims(extraClaims)
 				.setSubject(userdetails.getUsername())
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + (1000 * 30)))
+				.setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 30)))
 				.signWith(getSignInKey(),SignatureAlgorithm.HS256)
 				.compact();
 	}

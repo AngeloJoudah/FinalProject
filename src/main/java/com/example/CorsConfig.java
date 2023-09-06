cf8ce31869db8bdc09e5f8890f0e2e6a8212015b
AngeloJ123 <104799675+AngeloJ123@users.noreply.github.com>
2023-09-06 14:17:20 -0400

    cors for testing

--- src/main/java/com/example/CorsConfig.java
+++ src/main/java/com/example/CorsConfig.java
@@ -9,8 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
    @Override
     public void addCorsMappings(CorsRegistry registry) {
+	  
         registry.addMapping("/**")
-        		.allowedOriginPatterns("https://finalprojectangelo.azurewebsites.net/**")
+        		.allowedOriginPatterns("https://finalprojectangelo.azurewebsites.net/**","http://localhost:3000/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)

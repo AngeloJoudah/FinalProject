e17bda59b2d315e3ad14ae40f1abb2f0874a6a10
AngeloJ123 <104799675+AngeloJ123@users.noreply.github.com>
2023-09-12 13:23:48 -0400

    Fix dockerfile

--- courseapi/Mongo/MongoConfig.ts
+++ courseapi/Mongo/MongoConfig.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 const url =
-`mongodb://docdbfp:Ols5yE5Gqvn0ikyzcRkccAa8HC7gq8ASNm329sdpuaJKEl15ruuxV26fj4H3BValc4TfOxO7WzSGACDbtBu1iA%3D%3D@docdbfp.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@docdbfp@`
+process.env.MG_URL || ""
 export const connect = async() => {
     mongoose.set('strictQuery',false)
     const connection = await mongoose.connect(url)

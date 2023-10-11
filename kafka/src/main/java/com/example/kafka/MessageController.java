bf174fc3b1df521049a3f49609914b8d69ba28ca
AngeloJ123 <104799675+AngeloJ123@users.noreply.github.com>
2023-10-11 17:42:17 -0400

    great success

--- kafka/src/main/java/com/example/kafka/MessageController.java
+++ kafka/src/main/java/com/example/kafka/MessageController.java
@@ -1,11 +1,13 @@
 package com.example.kafka;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
 @RestController
 @RequestMapping("api/v3/messages")
 public class MessageController {
@@ -13,10 +15,15 @@ public class MessageController {
 	public MessageController(KafkaTemplate<String,String> kafkaTemplate) {
 		this.kafkaTemplate = kafkaTemplate;
 	}
-	@PostMapping
-	public void publish(@RequestBody MessageRequest req) {
+	@PostMapping("/post")
+	public ResponseEntity<String> publish(@RequestBody MessageRequest req) {
 		kafkaTemplate.send("messages",req.key(),req.message());
 	}
+	@GetMapping("/get")
+	public ResponseEntity<String> getMessages(@RequestBody MessageRequest req){
+		kafkaTemplate.receive()
+	}
+
 
 	
 }
